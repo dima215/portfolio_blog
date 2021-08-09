285c5da342ce6d2924c2ac5f163d6147c15ddd48
Dima215 <dimaogar215@gmail.com>
2021-08-09 23:36:36 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+//SLIDER SLICK
+
+$(document).ready(function () {
+  $(".top-slider").slick({
+    //autoplay: true,
+    //autoplaySpeed: 3000,
+    dots: true,
+    arrows: false,
+    fade: true,
+  });
+});
